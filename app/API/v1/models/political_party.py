57e3c37895b738c4458c3126c7a7e3034e301b27
dennis belio <dbelio39@gmail.com>
2019-02-06 21:58:16 +0300

    [Starts

---
+++ app/API/v1/models/political_party.py
@@ -0,0 +1,18 @@
+partys = []
+
+class PartysModel:
+  def __init__(self, name, hq_address, logo_url):
+    self.partys = partys
+    self.name = name
+    self.hq_address = hq_address
+    self.logo_url = logo_url
+
+  def save(self, name, hq_address, logo_url):
+    new_party = {
+      "id": len(self.partys) +1,
+      "name": name,
+      "hq_address": hq_address,
+      "logo_url": logo_url,
+    }  
+    partys.append(new_party)
+    return new_party
